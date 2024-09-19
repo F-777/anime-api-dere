b6f2873fe714a438f666e1933c828c0b7280fa2a
Admojo Revo 529 <imanuelrevoadmojo@gmail.com>
2024-09-19 10:24:32 +0700

    first commit

---
+++ src/Components/AddToList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const AddToList = () => {
+  return (
+    <>
+      <div className="mylist">
+        <p>Add To List +</p>
+      </div>
+    </>
+  );
+};
+
+export default AddToList;
